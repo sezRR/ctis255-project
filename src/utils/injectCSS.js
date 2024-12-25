60bb8d052baac36220b9e4ac79b68f678b74e174
sezRR <sezer.tetik2005@gmail.com>
2024-12-25 16:42:17 +0300

    injectCSS util file updated

--- src/utils/injectCSS.js
+++ src/utils/injectCSS.js
@@ -1,9 +1,15 @@
-function injectCSS(path) {
+/**
+* Injects a CSS file into the head of the document
+* @param {string} path - The path of the CSS file to be injected
+* @param {boolean} inLayout - Indicates if the CSS file is a layout CSS file
+*/
+function injectCSS(path, inLayout = false) {
     if (!$(`link[href="${path}"]`).length) {
         $('<link>')
             .attr('rel', 'stylesheet')
             .attr('href', path)
             .attr('data-dynamic', true)
+            .attr('data-layout', inLayout)
             .appendTo('head');
     }
 }
@@ -12,4 +18,12 @@ function removeCSS(path) {
     $(`link[href="${path}"][data-dynamic="true"]`).remove();
 }
 
-export { injectCSS, removeCSS };
+/**
+ * Removes all injected CSS files
+ * @param {string} exceptAttr - The attribute to exclude from removal
+ */
+function removeInjectedCSS(exceptAttr = 'data-layout') {
+    $(`link[data-dynamic="true"]:not([${exceptAttr}])`).remove();
+}
+
+export { injectCSS, removeCSS, removeInjectedCSS };
