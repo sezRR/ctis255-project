4a700c5abd160f3f895df6c123631e43a096e7f8
sezRR <sezer.tetik2005@gmail.com>
2024-12-25 16:44:29 +0300

    profiles page structure updated

---
+++ src/pages/profiles-page/profiles-page.js
@@ -0,0 +1,109 @@
+import User from "../../models/user.js";
+import { injectCSS } from "../../utils/injectCSS.js";
+import { addItemToLocalStorage } from "../../utils/storageUtil.js";
+import TemplateCache from "../../utils/templateCache.js";
+import globalState from "../../stores/globalState.js";
+import CryptocurrencyPage from "../cryptocurrency-page/cryptocurrency-page.js";
+import HeaderComponent from "../../components/header/header.js";
+
+const ProfilesPage = (function () {
+    const cssPath = 'src/pages/profiles-page/profiles-page.css';
+    const htmlPath = 'src/pages/profiles-page/profiles-page.html';
+
+    function render() {
+        injectCSS(cssPath);
+
+        TemplateCache.getTemplate(htmlPath, function (template) {
+            $(document).ready(function () {
+                const users = JSON.parse(localStorage.getItem("users")) || [];
+                if (users.length)
+                    $('.user-part').css("display", "none");
+                for (const user of users) {
+                    const profileCardHtml = profileCard(user.name);
+                    // Add data-id attribute to the profile card
+                    const $profileCardHtml = $(profileCardHtml); // convert the string to a jQuery object
+                    $($profileCardHtml).attr("data-id", user.id);
+                    $('.profile-cards').append($profileCardHtml);
+                }
+                $('.new-profile').click(function () {
+                    $('.overlay').fadeIn();
+                    $(".overlay").toggleClass("overlay-active");
+
+                });
+                $(".overlay").click(function () {
+                    $('.overlay').fadeOut();
+                    $(".overlay").fadeOut(function () {
+                        $(".overlay").toggleClass("overlay-active");
+                    });
+                });
+                $(".modal").click(function (e) {
+                    e.stopPropagation();
+                });
+                $(".modal button").click(function () {
+                    addProfile()
+                    $('.overlay').fadeOut();
+                    $(".overlay").fadeOut(function () {
+                        $(".overlay").toggleClass("overlay-active");
+                    });
+                })
+
+                $('.profile-cards').on("click", '.delete-btn', function (e) {
+                    e.stopPropagation();
+                    $(this).closest('.user-card').fadeOut(function () {
+                        $(this).remove();
+                        let users = JSON.parse(localStorage.getItem("users")) || [];
+                        users = users.filter(user => user.id !== $(this).data("id"));
+
+                        if (!users.length)
+                            $('.user-part').css("display", "block");
+
+                        localStorage.setItem("users", JSON.stringify(users));
+                    });
+                })
+
+                $(".profile-cards").on("click", ".user-card", function () {
+                    const userId = $(this).data("id");
+                    const user = JSON.parse(localStorage.getItem("users")).find(user => user.id === userId);
+                    globalState.currentUser = user;
+                    CryptocurrencyPage.render();
+                })
+            })
+
+            $("#content").html(template);
+        })
+    }
+
+    return { render }
+})();
+
+
+const profileCard = (profileName) =>
+    `
+    <div class="user-card">
+        <span class="delete-btn">x</span>
+        <i class="fa-solid fa-user"></i>
+        <div>${profileName}</div>
+    </div>
+`;
+
+function addProfile() {
+    const profileName = $('.modal input').val();
+
+    if (profileName) {
+        const createdUser = new User(profileName);
+        const profileCardHtml = profileCard(createdUser.name);
+
+        // Add data-id attribute to the profile card
+        const $profileCardHtml = $(profileCardHtml);
+        $($profileCardHtml).attr("data-id", createdUser.id);
+
+
+        $('.profile-cards').append($profileCardHtml);
+        $('.modal input').val('');
+        $(".overlay").fadeOut();
+        $('.user-part').css("display", "none");
+        addItemToLocalStorage("users", createdUser);
+    }
+}
+
+export default ProfilesPage;
