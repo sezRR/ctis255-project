4ead71a6912df195b0d6c31a902e0723e94700fa
sezRR <sezer.tetik2005@gmail.com>
2024-12-25 16:36:55 +0300

    core files of the project updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import HeaderComponent from "./components/header/header.js";
 import TemplateCache from "./utils/templateCache.js";
+import ProfilesPage from "./pages/profiles-page/profiles-page.js";
 
 $(document).ready(function () {
     // Initialize the Dashboard View
@@ -9,5 +10,7 @@ $(document).ready(function () {
 
         // Initialize the Header Component
         HeaderComponent.render('#header', 'CTIS', "Crypto Trading Information System"); // TODO: Get the title and description from the config file
+
+        ProfilesPage.render()
     })
 });
