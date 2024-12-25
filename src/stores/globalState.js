08846bbdf63c64b12c374f2401ec9c70b0202e05
sezRR <sezer.tetik2005@gmail.com>
2024-12-25 16:33:57 +0300

    documentation added to the globalState.js file

--- src/stores/globalState.js
+++ src/stores/globalState.js
@@ -1,3 +1,17 @@
+/**
+ * This file is used to store the global state of the application.
+ * 
+ * The global state is used to store data that is shared between multiple components.
+ * Here is the current document structure:
+ * {
+ *    currentUser: {
+ *       id: "sample-uuid",
+ *       name: "Sezer",
+ *       ...
+ *    }
+ * }
+ */
+
 let states = {} // This is the global state object
 
 export default states
